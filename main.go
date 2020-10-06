af1094e742b70b9224500e9d158b7d7a09e56e2f
Gnoale <g.noale@gmail.com>
2020-10-06 15:58:13 +0200

    try gorilla/mux

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fsdb := &FileDB{"db.txt"}
 	fsdb.Init()
 	app := &App{fsdb}
-	user := &User{}
-	Creds = map[string]string{"john": "secret"}
-	http.HandleFunc("/item/", user.authHandler(app.itemHandler))
-	http.ListenAndServe(":8080", nil)
+	//user := &User{}
+	//Creds = map[string]string{"john": "secret"}
+	r := mux.NewRouter()
+	r.HandleFunc("/item", app.newItem).Methods("POST")
+	r.HandleFunc("/item/{name}", app.getItem).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
 }
--- server.go
+++ server.go
@@ -4,7 +4,8 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
-	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 type App struct {
@@ -44,41 +45,36 @@ func (u *User) authHandler(next func(w http.ResponseWriter, r *http.Request)) fu
 	}
 }
 
-func (app *App) itemHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	// new item
-	case http.MethodPost:
-		defer r.Body.Close()
-		item := NewItem()
-		// "validation"
-		dec := json.NewDecoder(r.Body)
-		dec.DisallowUnknownFields()
-		if err := dec.Decode(item); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		// insertion
-		if err := app.db.InsertItem(item); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusCreated)
-	case http.MethodGet:
-		// TODO url sanitization ?
-		name := strings.Split(r.URL.Path, "/")[2]
-		item, err := app.db.GetItem(name)
-		// TODO inspect error
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusNotFound)
-			return
-		}
-		// write json
-		js, err := json.Marshal(item)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(js)
+func (app *App) newItem(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	item := NewItem()
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	if err := dec.Decode(item); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+	if err := app.db.InsertItem(item); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (app *App) getItem(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["name"]
+	item, err := app.db.GetItem(name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	// write json
+	js, err := json.Marshal(item)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
